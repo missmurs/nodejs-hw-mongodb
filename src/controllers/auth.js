eaaf896f770515b22dca9036cf542e35529cc14b
missmurs <131698859+missmurs@users.noreply.github.com>
2024-11-12 21:46:27 +0200

    getGoogleOAuthUrlController

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,7 +5,7 @@ import { logoutUser } from '../services/auth.js';
 import { refreshUsersSession } from '../services/auth.js';
 import { resetPassword } from '../services/auth.js';
 import { requestResetToken } from '../services/auth.js';
-
+import { generateAuthUrl } from '../utils/googleOAuth2.js';
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
@@ -87,5 +87,15 @@ export const resetPasswordController = async (req, res) => {
     message: 'Password has been successfully reset.',
     status: 200,
     data: {},
+  });
+};
+export const getGoogleOAuthUrlController = async (req, res) => {
+  const url = generateAuthUrl();
+  res.json({
+    status: 200,
+    message: 'Successfully get Google OAuth url!',
+    data: {
+      url,
+    },
   });
 };
